406e36108de2cd691c3a9cef7e2ffcadda7edfb2
KaranMishra <93134411+Karan-Kumar-Mishra@users.noreply.github.com>
2024-06-20 11:36:39 +0530

    Delete Online-compiler directory

--- Online-compiler/Code/code.c
+++
@@ -1,6 +0,0 @@
-using namespace std;
-int main()
-{
-    cout<<"hello word"<<endl;
-    return 0;
-}
--- Online-compiler/code.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-cout<<"hello sir i am zack"<<endl;
-return 0;
-}
